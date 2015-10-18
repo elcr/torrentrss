9243aabeefca254683d2fe657adf80d60ebf2c27
elcr <elcr@outlook.com>
2015-10-18 14:57:29 +0100

    setup.py, also TODOs

---
+++ setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+setuptools.setup(
+    name='torrentrss',
+    description='torrentrss',
+    version='0.1',
+    author='elcr',
+    url='https://bitbucket.org/elcr/torrentrss',
+    license='MIT',
+    install_requires=['click', 'requests', 'feedparser', 'jsonschema'],
+    packages=['torrentrss'],
+    package_data={'torrentrss': ['config_example.json', 'config_schema.json']},
+    entry_points={'console_scripts': ['torrentrss=torrentrss.cli:main']}
+)
